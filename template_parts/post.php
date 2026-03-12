e0c418e2d4aeefe462cc346aaf8b02de5aa71761
Jose Hz <solware.eirl@gmail.com>
2026-03-12 14:54:58 -0500

    corrigiendo author

--- template_parts/post.php
+++ template_parts/post.php
@@ -11,7 +11,7 @@
             <p class="meta">
                 <span>Por: </span>
                 <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
-                    <?php echo get_the_author_meta('display_name'); ?>
+                    <?php echo get_the_author('display_name'); ?>
                 </a>
                 
             </p>
